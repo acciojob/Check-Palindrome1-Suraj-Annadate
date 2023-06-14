2f03d4d1764b56f27d40dbf9505ed7bda102c032
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-06-14 11:57:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase
+	let i=0
+	let j = str.length-1
 
+	while (i<j) {
+		if(str.charAt(i)!=str.charAt(j))
+		{
+			return false
+		}
+		i++
+		j--
+	}
+	return true
 }
 module.exports = palindrome
